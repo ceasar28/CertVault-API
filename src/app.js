fe169795faaf9d0f5b94e4d81dee0486fbd174c2
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-04-22 01:27:00 +0100

    express server

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
+
 //Middlewares
+
+// soin up the server
+app.listen(PORT, () => {
+  return console.log(`Server is listening on port:${PORT}`);
+});
