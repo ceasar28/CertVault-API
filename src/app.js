d261d6c743f0582d264e0f5947336cad49942561
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-04-22 18:22:18 +0100

    database config

--- src/app.js
+++ src/app.js
@@ -8,3 +8,19 @@ const PORT = 5000;
 app.listen(PORT, () => {
   return console.log(`Server is listening on port:${PORT}`);
 });
+
+const connectDb = require("../config/database");
+const serverUp = async () => {
+  try {
+    const connected = await connectDb("mongodb://27017/certVault_DB");
+    app.listen(PORT, () => {
+      return console.log(`Server is listening on port:${PORT}`);
+    });
+
+    if (connected) {
+      console.log("Database connected");
+    } else console.log("Database not connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
