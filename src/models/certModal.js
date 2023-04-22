d5bc1e427dd24f8834d60266b593dd0d6bf9af87
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-04-22 09:22:36 +0100

    model

--- src/models/certModal.js
+++ src/models/certModal.js
@@ -1,14 +1,10 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  user: {
-    type: String,
+  certificate: {
+    type: Array,
     require: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
   },
 });
 
-module.exports = new mongoose.model("user", userSchema);
+module.exports = new mongoose.model("certificate", userSchema);
