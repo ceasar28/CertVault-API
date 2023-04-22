3c8c415fabf5a0f2d2e6efdfb81d6f903899d943
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-04-22 08:48:50 +0100

    model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,1 +1,11 @@
 const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  user: {
+    type: String,
+    require: true,
+  },
+  certificates: {
+    type: Array,
+  },
+});
